cd018b37462df4b66a8c9ba7a54ce68b465ce82f
allenmurfee <allenmurfee123@gmail.com>
2022-08-26 13:27:11 -0400

    started generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,37 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate Password Function
+function generatePassword() {
+  var passLength = prompt("How long do you want your password to be? Between 8 and 128 character.");
+  
+  if (passLength <= 7 || >= 129) {
+    return alert("Must be between 8 and 128 characters."); 
+  }
+ 
+  var lowerCase = confirm("Do you want lowercase letters in your password?");
+  if (!lowerCase) {
+    return; 
+  }
+  
+  var upperCase = confirm("Do you want uppercase letters in your password?");
+  if (!upperCase) {
+    return; 
+  }
+
+  var numeric = confirm("Do you want numbers in your password?");
+  if (!numeric) {
+    return; 
+  }
+
+  var specialChar = confirm("Do you want special characters in your password?");
+  if (!specialChar) {
+    return; 
+  }
+}
+
